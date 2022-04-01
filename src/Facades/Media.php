5d58f0736362e4e1074b3d42ecbe7026bfc65364
Apsonex Inc <apsonexinc@gmail.com>
2022-04-01 14:48:46 -0400

    update

--- src/Facades/Media.php
+++ src/Facades/Media.php
@@ -13,7 +13,7 @@ use Intervention\Image\Image;
  * @method static ImageFactory imageFactory(Image|UploadedFile|string $src, string $targetPath = null, string $disk = null)
  * @method static ImageOptimizeAction optimizeImage(string $srcDisk, string $srcPath, string $srcTarget = null, string $targetDisk = null, mixed $callback = null)
  * @method static array makeImageVariations(string $path, array $variations, string $srcDisk, string $targetDisk = null, $callback = null)
- * @method static bool deleteVariations(string $disk, array $variations, mixed $callback = null)
+ * @method static bool deleteImageVariations(string $disk, array $variations, mixed $callback = null)
  */
 class Media extends Facade
 {
